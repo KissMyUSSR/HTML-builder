ac76edd490958605bb8c08ddb7b2c3d77e177122
Kirill Cherepanov <cherkirr@gmail.com>
2021-11-08 15:33:05 +0300

    fix: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,20 +8,29 @@ const copyPath = path.join(__dirname, 'files_copy');
 fs.access(folderPath, e => { if (e) throw e; });
 
 fs.access(copyPath, err => {
-  if (!err) fs.writeFile(copyPath, '', ()=>{
+  if (!err) fs.rm(copyPath, { recursive: true, force: true }, err => {
+    if (err) throw err;
     recurCopy(folderPath, copyPath);
   });
   else recurCopy(folderPath, copyPath);
 });
 
 function recurCopy(fPath, cPath) {
-  mkdir(cPath);
-  readdir(fPath)
-    .then(files => files.forEach(file => {
-      fs.stat(path.join(fPath, file), (err, stats) => {
-        if (err) throw err;
-        if (!stats.isFile()) return recurCopy(path.join(fPath, file), path.join(cPath, file));
-        fs.copyFile(path.join(fPath, file), path.join(cPath, file), (e)=>{if (e) throw e; });
-      });
-    }));
+  mkdir(cPath)
+    .then((files, err) => {
+      if (err) throw err;
+      
+      readdir(fPath)
+        .then((files, err) => files.forEach(file => {
+          if (err) throw err;
+
+          fs.stat(path.join(fPath, file), (err, stats) => {
+            if (err) throw err;
+
+            if (!stats.isFile()) return recurCopy(path.join(fPath, file), path.join(cPath, file));
+
+            fs.copyFile(path.join(fPath, file), path.join(cPath, file), (e)=>{ if (e) throw e; });
+          });
+        }));
+    });
 }
